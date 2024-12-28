f494e6d21905425467c9c5a8819159dc0311dc53
Joseph Samuel <mrsamd02@gmail.com>
2024-12-28 11:48:57 +0100

    k

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default [
       ...react.configs["jsx-runtime"].rules,
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/prop-types": "off",
     },
   },
 ];
